1989cc69a35849c168245942809456fa0ce0325e
JonnyTran <JonnyTran@github.com>
2018-10-30 05:03:34 -0500

    added starbase rna-rna interaction network (filtered)

--- TCGAMultiOmics/genomic.py
+++ TCGAMultiOmics/genomic.py
@@ -472,6 +472,7 @@ class GeneExpression(GenomicData):
             "pairGeneName"].str.lower()
         df.loc[df["pairGeneType"] == "miRNA", "pairGeneName"] = df[df["pairGeneType"] == "miRNA"][
             "pairGeneName"].str.replace("-3p.*|-5p.*", "")
+        df = df[df["expNum"] > 1]
 
         self.starBase_RNA_RNA_network = nx.from_pandas_edgelist(df, source='geneName', target='pairGeneName',
                                                                 edge_attr=["interactionNum"],
