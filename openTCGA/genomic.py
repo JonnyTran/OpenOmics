314ec0e2a82c287e650916b16519fdd6cc67e12a
Jonny Tran <nhat.tran@mavs.uta.edu>
2019-08-27 01:38:39 -0500

    MIR and LNC annotations working

--- openTCGA/genomic.py
+++ openTCGA/genomic.py
@@ -1,15 +1,15 @@
 from openTCGA.expression import ExpressionData
 
 class SomaticMutation(ExpressionData):
-    def __init__(self, cohort_name, file_path=True:
-        super().__init__(cohort_name, file_path,,
+    def __init__(self, cohort_name, file_path=True):
+        super().__init__(cohort_name, file_path)
 
 
 class DNAMethylation(ExpressionData):
-    def __init__(self, cohort_name, file_path=True:
-        super().__init__(cohort_name, file_path,,
+    def __init__(self, cohort_name, file_path=True):
+        super().__init__(cohort_name, file_path)
 
 
 class CopyNumberVariation(ExpressionData):
-    def __init__(self, cohort_name, file_path=True:
-        super().__init__(cohort_name, file_path,,
+    def __init__(self, cohort_name, file_path=True):
+        super().__init__(cohort_name, file_path)
