b00d4c349147e798993f00c789dbe4c9ce2cb790
JonnyTran <JonnyTran@github.com>
2019-08-31 20:25:01 -0500

    Bump version: 0.6.4 → 0.6.5

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ class UploadCommand(Command):
 
 setup(
     name=NAME,
-    version="0.6.4",
+    version="version='0.6.5'",
     packages=find_packages(include=['openomics']),
     package_dir={NAME: 'openomics'},
     url=URL,
