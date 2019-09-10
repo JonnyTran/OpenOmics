c9a82dac96c4727e623c1554d701c17ff42089b0
Jonny Tran <nhat.tran@mavs.uta.edu>
2019-09-09 19:21:59 -0500

    Bump version: 0.7.4 → 0.7.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.rst') as history_file:
 
 # Package meta-data.
 NAME = 'openomics'
-version = '0.7.4'
+version = 'version='0.7.5''
 DESCRIPTION = 'OpenOmics provides a bioinformatics API and web-app platform integrate and visualize the multiomics and clinical data.'
 URL = 'https://github.com/JonnyTran/OpenOmics'
 EMAIL = 'nhat.tran@mavs.uta.edu'
