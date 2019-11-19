ef8708d41cc0904a19e7bd2d00edc6052bfaf5de
Jonny Tran <nhat.tran@mavs.uta.edu>
2019-11-19 16:03:33 -0600

    refactored dash app

--- openomics_web/app.py
+++ openomics_web/app.py
@@ -1,13 +1,15 @@
 import dash
 import dash_bootstrap_components as dbc
 
-from layout import app_layout
+from openomics_web.layouts import app_layout
+from openomics_web.server import server
 
 # running directly with Python
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
+app = dash.Dash(__name__,
+                server=server,
+                external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = app_layout()
+app.layout = app_layout.app_main()
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8050)
