8b74e88ed28b264aaa8a892a7deea1f847b8de8f
Jonny Tran <nhat.tran@mavs.uta.edu>
2019-12-03 14:57:24 -0600

    latest

--- openomics_web/app.py
+++ openomics_web/app.py
@@ -1,8 +1,12 @@
+import base64
+import os
+
 import dash
+import pandas as pd
 from dash.dependencies import Input, Output, State
 
 from openomics_web.layouts import app_layout
-from openomics_web.layouts.datatable_upload import parse_datatable_file
+from openomics_web.layouts.datatable_upload import parse_datatable_file, DataTableColumnSelect
 from openomics_web.server import server
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -15,19 +19,38 @@ app = dash.Dash(__name__,
 app.layout = app_layout.app_main()
 
 
-@app.callback([Output('data_table_columns_select', 'options')],
+@app.callback([Output('upload_table_preview', 'children'),
+               Output('upload-data', 'children')],
               [Input('upload-data', 'contents'),
                Input('upload-data', 'filename')],
               [State('data-table-type', 'value'), ])
-def update_datatable_upload(list_of_contents, list_of_names, cohort_name, data_type, ):
-    print("cohort_name", cohort_name)
-    print("list_of_names", list_of_names)
-    print("data_type", data_type)
-    print("list_of_contents", list_of_contents)
+def update_datatable_metadata(list_of_contents, list_of_names, data_type, ):
+    if list_of_contents is None:
+        return None
 
-    children = [parse_datatable_file(cohort_name, c, n, data_type) for c, n in zip(list_of_contents, list_of_names)]
+    try:
+        content = list_of_contents[0]
+        filename = list_of_names[0]
+        _, file_extension = os.path.splitext(filename)
 
-    return
+        content_type, content_string = content.split(',')
+        decoded = base64.b64decode(content_string)
+        first_line = decoded.decode('utf-8').partition('\n')[0]
+
+        if file_extension == ".tsv":
+            columns = first_line.split('\t')
+        elif file_extension == ".csv":
+            columns = first_line.split(',')
+        elif file_extension == ".xls":
+            columns = pd.read_excel(decoded.decode('utf-8')).columns.tolist()
+        else:
+            columns = first_line.split(',')
+
+    except Exception as e:
+        print(e)
+        return 'There was an error processing this file.'
+
+    return DataTableColumnSelect(columns), "Uploaded {}".format(list_of_names)
 
 
 @app.callback(Output('output-data-upload', 'children'),
@@ -36,13 +59,13 @@ def update_datatable_upload(list_of_contents, list_of_names, cohort_name, data_t
                State('data-table-type', 'value'),
                State('upload-data', 'contents'),
                State('upload-data', 'filename'),
-               State('data_table_columns_select', 'value')
+               # State('data_table_columns_select', 'value')
                ])
-def import_datatable_upload(n_clicks, cohort_name, data_type, list_of_contents, list_of_names, columns):
+def import_datatable_upload(n_clicks, cohort_name, data_type, list_of_contents, list_of_names):
     if list_of_contents is None:
-        raise Exception()
+        return []
 
-    print("columns", columns)
+    # print("columns", columns)
 
     children = [parse_datatable_file(cohort_name, c, n, data_type) for c, n in zip(list_of_contents, list_of_names)]
     return children
