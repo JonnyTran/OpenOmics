88c98dcbc75acdaf28728047c4347b3bece3f84c
JonnyTran <jonnytran@github.com>
2019-12-07 12:45:21 -0600

    latest

--- openomics_web/layouts/datatable_view.py
+++ openomics_web/layouts/datatable_view.py
@@ -1,6 +1,7 @@
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table as dt
+import pandas as pd
 
 from openomics_web.utils.str_utils import longest_common_prefix
 
