a90cf7cb61f601cb2a233ad2fd41300211d85862
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-01-04 09:47:43 -0600

    added GTEx

--- openomics/database/__init__.py
+++ openomics/database/__init__.py
@@ -2,7 +2,7 @@ from __future__ import print_function, division, absolute_import
 
 try:
     from .annotation import (
-        Annotatable, GENCODE, MirBase, EnsemblGenes, RNAcentral, BioMartManager
+        Annotatable, GENCODE, MirBase, EnsemblGenes, RNAcentral, BioMartManager, GTEx
     )
 
     from .interaction import (
