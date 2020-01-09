9c3bbe444e9455cac712bf857950fbab19f18b14
JonnyTran <jonnytran@github.com>
2020-01-08 22:30:48 -0600

    bumped to 0.7.9

--- openomics/__init__.py
+++ openomics/__init__.py
@@ -1,11 +1,12 @@
 from __future__ import print_function, division, absolute_import
+
 # -*- coding: utf-8 -*-
 
 """Top-level package for openomics."""
 
 __author__ = """Nhat (Jonny) Tran"""
 __email__ = 'nhat.tran@mavs.uta.edu'
-__version__ = '__version__ = '__version__ = '__version__ = '0.7.9''''
+__version__ = '0.7.9'
 
 try:
     # from . import database
