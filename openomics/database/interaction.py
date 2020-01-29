e0287f152e4dd2bea11adf5d54f99746d7530fb4
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-01-29 16:44:25 -0600

    latest

--- openomics/database/interaction.py
+++ openomics/database/interaction.py
@@ -50,7 +50,7 @@ class Interactions(Dataset):
     def load_network(self, file_resources, source_col_name, target_col_name, edge_attr, directed):
         raise NotImplementedError
 
-    def get_interactions(self, nodelist, data=False, inclusive=True):
+    def get_interactions(self, nodelist=None, data=False, inclusive=True):
         """
 
         Args:
@@ -65,10 +65,13 @@ class Interactions(Dataset):
             edges (OutEdgeView): a NetworkX edgelist
         """
         if hasattr(self, "network"):
+            if nodelist is None:
+                return self.network.edges(data=data)
+
             if inclusive:
                 return self.network.subgraph(nodelist).edges(data=data)
             else:
-                return self.network.edges(nodelist=nodelist, data=data)
+                return self.network.edges(nbunch=nodelist, data=data)
 
         else:
             raise Exception(
