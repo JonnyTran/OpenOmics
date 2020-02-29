98edd0d0d2c129a882e0b35accd1068989ba18b4
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-02-29 16:11:13 -0600

    refactored MirBase

--- openomics/utils/df.py
+++ openomics/utils/df.py
@@ -1,8 +1,18 @@
 
-
 def concat_uniques(df_column):
+    before_shape = df_column.shape
     df_column = df_column.dropna().astype(str)
+    assert before_shape == df_column.shape
+
     if not df_column.empty:
         return "|".join(df_column.unique())
+    else:
+        return None
+
+
+def concat(df_column):
+    df_column = df_column.dropna().astype(str)
+    if not df_column.empty:
+        return "|".join(df_column)
     else:
         return None
