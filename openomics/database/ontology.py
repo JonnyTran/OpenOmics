72a4439de265bb534a1972c0e2b00ef51e626e28
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-03-19 17:14:42 -0500

    updated GeneOntology

--- openomics/database/ontology.py
+++ openomics/database/ontology.py
@@ -1,11 +1,14 @@
+import numpy as np
+import obonet
 import pandas as pd
 from Bio.UniProt import GOA
 from goatools.obo_parser import OBOReader
 
 from .base import Dataset
+from .interaction import Interactions
 
 
-class GeneOntology(Dataset):
+class GeneOntology(Dataset, Interactions):
     COLUMNS_RENAME_DICT = {
         "DB_Object_Symbol": "gene_name",
         "DB_Object_ID": "gene_id",
@@ -15,29 +18,58 @@ class GeneOntology(Dataset):
     def __init__(self, path="http://geneontology.org/gene-associations/",
                  file_resources=None, col_rename=COLUMNS_RENAME_DICT, npartitions=0):
         if file_resources is None:
-            file_resources = {"goa_human.gaf": "goa_human.gaf.gz",
-                              "go-basic.obo": "http://purl.obolibrary.org/obo/go/go-basic.obo",
-                              "goa_human_rna.gaf": "goa_human_rna.gaf.gz",
-                              "goa_human_isoform.gaf": "goa_human_isoform.gaf.gz"
-                              }
+            file_resources = {
+                "go-basic.obo": "http://purl.obolibrary.org/obo/go/go-basic.obo",
+                "goa_human.gaf": "goa_human.gaf.gz",
+                "goa_human_rna.gaf": "goa_human_rna.gaf.gz",
+                "goa_human_isoform.gaf": "goa_human_isoform.gaf.gz"
+            }
         super(GeneOntology, self).__init__(path, file_resources, col_rename=col_rename, npartitions=npartitions)
 
+        self.network = self.load_network(file_resources=file_resources, source_col_name=None,
+                                         target_col_name=None,
+                                         edge_attr=None, directed=True)
+
     def load_dataframe(self, file_resources):
-        lines = []
-        dfs = []
-        for file in self.file_resources:
+        go_annotation_dfs = []
+        for file in file_resources:
             if ".obo" in file:
                 go_terms = []
-                for term in OBOReader(self.file_resources[file]):
-                    go_terms.append({"id": term.id, "name": term.name, "namespace": term.namespace})
-                self.go_terms = pd.DataFrame(go_terms)
+                for term in OBOReader(file_resources[file]):
+                    go_terms.append({"id": term.id, "go_name": term.name, "namespace": term.namespace})
+                go_terms = pd.DataFrame(go_terms)
+
             elif ".gaf" in file:
-                for line in GOA.gafiterator(self.file_resources[file]):
-                    lines.append(line)
-                go_df = pd.DataFrame(lines)
-                dfs.append(go_df)
+                go_lines = []
+                for line in GOA.gafiterator(file_resources[file]):
+                    go_lines.append(line)
+                go_annotation_dfs.append(pd.DataFrame(go_lines))
             else:
                 raise Exception("file_resources[{}] must either be .gaf or .obo".format(file))
 
-        go_df = pd.concat(dfs)
-        return go_df
+        go_annotations = pd.concat(go_annotation_dfs)
+        go_annotations["go_name"] = go_annotations["go_id"].map(go_terms.set_index("id")["go_name"])
+
+        return go_annotations
+
+    def load_network(self, file_resources, source_col_name=None, target_col_name=None, edge_attr=None, directed=True):
+        file_resources["go-basic.obo"].seek(0)
+        go_graph = obonet.read_obo(file_resources["go-basic.obo"])
+        return go_graph
+
+    def add_predecessor_terms(self, annotations: pd.Series, return_str=True):
+        if annotations.dtypes == np.object and annotations.str.contains("\||;", regex=True).any():
+            go_terms_annotations = annotations.str.split("|")
+        else:
+            go_terms_annotations = annotations
+
+        go_terms_parents = go_terms_annotations.map(
+            lambda terms: list(set(terms) | {parent for term in terms \
+                                             for parent in self.network.predecessors(term)}) \
+                if isinstance(terms, list) else None)
+
+        if return_str:
+            go_terms_parents = go_terms_parents.map(
+                lambda x: "|".join(x) if isinstance(x, list) else None)
+
+        return go_terms_parents
