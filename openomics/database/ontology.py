be5a6abb98cbd646d4cde9368e92c8545c759304
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-03-19 18:23:38 -0500

    `updated GeneOntology`

--- openomics/database/ontology.py
+++ openomics/database/ontology.py
@@ -3,7 +3,6 @@ import numpy as np
 import obonet
 import pandas as pd
 from Bio.UniProt import GOA
-from goatools.obo_parser import OBOReader
 
 from .base import Dataset
 
@@ -26,41 +25,31 @@ class GeneOntology(Dataset):
             }
         super(GeneOntology, self).__init__(path, file_resources, col_rename=col_rename, npartitions=npartitions)
 
-        self.network = self.load_network(file_resources=file_resources, source_col_name=None,
-                                         target_col_name=None,
-                                         edge_attr=None, directed=True)
-        print("{}".format(nx.info(self.network)))
+        print("go_graph {}".format(nx.info(self.go_graph)))
 
     def load_dataframe(self, file_resources):
         go_annotation_dfs = []
         for file in file_resources:
-            if ".obo" in file:
-                go_terms = []
-                for term in OBOReader(file_resources[file]):
-                    go_terms.append({"id": term.id, "go_name": term.name, "namespace": term.namespace})
-                go_terms = pd.DataFrame(go_terms)
-
-            elif ".gaf" in file:
+            if ".gaf" in file:
                 go_lines = []
                 for line in GOA.gafiterator(file_resources[file]):
                     go_lines.append(line)
                 go_annotation_dfs.append(pd.DataFrame(go_lines))
-            else:
-                raise Exception("file_resources[{}] must either be .gaf or .obo".format(file))
 
         go_annotations = pd.concat(go_annotation_dfs)
-        go_annotations["go_name"] = go_annotations["go_id"].map(go_terms.set_index("id")["go_name"])
+
+        for file in file_resources:
+            if ".obo" in file:
+                self.go_graph = obonet.read_obo(file_resources[file])
+                go_terms = pd.DataFrame.from_dict(self.go_graph.nodes, orient='index', dtype="object")
+                go_annotations["go_name"] = go_annotations["GO_ID"].map(go_terms["name"])
+                go_annotations["namespace"] = go_annotations["GO_ID"].map(go_terms["namespace"])
 
         return go_annotations
 
-    def load_network(self, file_resources, source_col_name=None, target_col_name=None, edge_attr=None, directed=True):
-        file_resources["go-basic.obo"].seek(0)
-        go_graph = obonet.read_obo(file_resources["go-basic.obo"])
-        return go_graph
-
     def get_predecessor_terms(self, annotation: pd.Series):
         go_terms_parents = annotation.map(
-            lambda terms: list({parent for term in terms for parent in self.network.predecessors(term)}) \
+            lambda terms: list({parent for term in terms for parent in self.go_graph.predecessors(term)}) \
                 if isinstance(terms, list) else None)
         return go_terms_parents
 
