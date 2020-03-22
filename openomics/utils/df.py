8c22998b139426952abef1cae3bd09132f741948
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-03-21 20:40:36 -0500

    updated GeneOntology

--- openomics/utils/df.py
+++ openomics/utils/df.py
@@ -15,3 +15,13 @@ def concat(series: pd.Series):
         return "|".join(series)
     else:
         return None
+
+
+def slice_adj(adj, node_list: list, nodes_A, nodes_B=None):
+    if nodes_B is None:
+        idx = [node_list.index(node) for node in nodes_A]
+        return adj[idx, :][:, idx]
+    else:
+        idx_A = [node_list.index(node) for node in nodes_A]
+        idx_B = [node_list.index(node) for node in nodes_B]
+        return adj[idx_A, :][:, idx_B]
