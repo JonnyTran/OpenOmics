8cc3d74cf6dd5c8f3c4464e922dadd39ab291d4e
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-05-01 23:42:16 -0500

    fix STRING

--- openomics/database/__init__.py
+++ openomics/database/__init__.py
@@ -1,26 +1,14 @@
 from __future__ import print_function, division, absolute_import
 
-try:
-    from .annotation import (
-        EnsemblGenes, RNAcentral, BioMartManager, GTEx
-    )
-    from .sequence import GENCODE, MirBase
-    from .ontology import GeneOntology
-
-    from .interaction import (
-        LncBase, MiRTarBase, TargetScan, LncRNA2Target, BioGRID, GeneMania, lncRInter, lncRNome, STRING
-    )
-
-    from .disease import (
-        DisGeNet, HMDD, LncRNADisease, MalaCards
-    )
-
-    from .base import Annotatable
-
-except ImportError as e:
-    msg = (
-        "OpenOmics requirements are not installed.\n\n"
-        "Please pip install as follows:\n\n"
-        "  pip install openomics --upgrade  # or pip install"
-    )
-    raise ImportError(str(e) + "\n\n" + msg)
+from .annotation import (
+    EnsemblGenes, RNAcentral, BioMartManager, GTEx
+)
+from .base import Annotatable
+from .disease import (
+    DisGeNet, HMDD, LncRNADisease, MalaCards
+)
+from .interaction import (
+    LncBase, MiRTarBase, TargetScan, LncRNA2Target, BioGRID, GeneMania, lncRInter, lncRNome, STRING
+)
+from .ontology import GeneOntology
+from .sequence import GENCODE, MirBase
