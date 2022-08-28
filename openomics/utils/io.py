335aad47921264d58e53a315eb9ff87e84c9924b
JonnyTran <JonnyTran@github.com>
2022-08-27 19:28:37 -0500

    Database.load_dataframe() now can retain pandas.set_index() Refactoring uncompress files func to utils/io.py UniProt added TrEMBL

--- openomics/utils/io.py
+++ openomics/utils/io.py
@@ -1,46 +1,83 @@
+import gzip
 import io
 import logging
 import os
+import zipfile
+from pathlib import Path
 from urllib.error import URLError
 
 import dask.dataframe as dd
+import rarfile
 import requests
 import sqlalchemy as sa
 import validators
 from astropy.utils import data
+from filetype import Type
 from requests.adapters import HTTPAdapter, Retry
 
 import openomics
 
 
 # @astropy.config.set_temp_cache(openomics.config["cache_dir"])
-def get_pkg_data_filename(dataurl, file):
+def get_pkg_data_filename(baseurl, filepath):
     """Downloads a remote file given the url, then caches it to the user's home
     folder.
 
     Args:
-        dataurl: Url to the download path, excluding the file name
-        file: The file path to download
+        baseurl: Url to the download path, excluding the file name
+        filepath: The file path to download
 
     Returns:
         filename (str): A file path on the local file system corresponding to
         the data requested in data_name.
     """
     # Split data url and file name if the user provided a whole path in file_resources
-    if validators.url(file):
-        dataurl, file = os.path.split(file)
-        dataurl = dataurl + "/"
+    if validators.url(filepath):
+        baseurl, filepath = os.path.split(filepath)
+        baseurl = baseurl + "/"
 
     try:
-        logging.debug("Fetching file from: {}{}, saving to {}".format(dataurl, file, openomics.config['cache_dir']))
+        logging.debug("Fetching file from: {}{}, saving to {}".format(baseurl, filepath, openomics.config['cache_dir']))
 
-        with data.conf.set_temp("dataurl", dataurl), data.conf.set_temp("remote_timeout", 30):
-            return data.get_pkg_data_filename(file, package="openomics.database", show_progress=True)
+        with data.conf.set_temp("dataurl", baseurl), data.conf.set_temp("remote_timeout", 30):
+            return data.get_pkg_data_filename(filepath, package="openomics.database", show_progress=True)
 
     except (URLError, ValueError) as e:
-        raise Exception(f"Unable to download file at {os.path.join(dataurl, file)}. "
+        raise Exception(f"Unable to download file at {os.path.join(baseurl, filepath)}. "
                         f"Please try manually downloading the files and add path to `file_resources` arg. \n{e}")
 
+
+def decompress_file(data_file: Path, filename: str, file_ext: Type):
+    # This null if-clause is needed when filetype_ext is None, causing the next clauses to fail
+    if file_ext is None:
+        output = data_file
+
+    elif file_ext.extension == "gz":
+        logging.debug("Decompressed gzip file at {}".format(data_file))
+        output = gzip.open(data_file, "rt")
+
+    elif file_ext.extension == "zip":
+        logging.debug("Decompressed zip file at {}".format(data_file))
+        zf = zipfile.ZipFile(data_file, "r")
+
+        for subfile in zf.infolist():
+            # If the file extension matches
+            if os.path.splitext(subfile.filename)[-1] == os.path.splitext(filename)[-1]:
+                output = zf.open(subfile.filename, mode="r")
+
+    elif file_ext.extension == "rar":
+        logging.debug("Decompressed rar file at {}".format(data_file))
+        rf = rarfile.RarFile(data_file, "r")
+
+        for subfile in rf.infolist():
+            # If the file extension matches
+            if os.path.splitext(subfile.filename)[-1] == os.path.splitext(filename)[-1]:
+                output = rf.open(subfile.filename, mode="r")
+    else:
+        print(f"WARNING: filepath_ext.extension {file_ext.extension} not supported.")
+        output = data_file
+
+    return output
 
 def read_db(path, table, index_col):
     """
