23b46cae72d6b4d9d635c70285c2d256fd6760d7
JonnyTran <JonnyTran@github.com>
2022-09-12 11:19:14 -0500

    Dask aggregation for GeneOntology to include parent GO terms working

--- openomics/database/ontology.py
+++ openomics/database/ontology.py
@@ -1,13 +1,15 @@
 import os
 import warnings
+from collections.abc import Iterable
 from io import TextIOWrapper, StringIO
-from typing import Tuple, List, Dict, Iterable, Union, Callable
+from typing import Tuple, List, Dict, Union, Callable
 
 import dask.dataframe as dd
 import networkx as nx
 import numpy as np
 import obonet
 import pandas as pd
+from networkx import NetworkXError
 from pandas import DataFrame
 
 from openomics.utils.adj import slice_adj
@@ -139,14 +141,12 @@ class Ontology(Database):
 
     def add_predecessor_terms(self, anns: pd.Series, edge_type: Union[str, List[str]] = 'is_a', sep="\||;"):
         anns_w_parents = anns.map(lambda x: [] if not isinstance(x, (list, np.ndarray)) else x) + \
-                         get_predecessor_terms(self.get_subgraph(edge_type), anns)
+                         get_predecessor_terms(anns, self.get_subgraph(edge_type))
 
         return anns_w_parents
 
     @staticmethod
-    def get_node_color(
-        file="~/Bioinformatics_ExternalData/GeneOntology/go_colors_biological.csv",
-    ):
+    def get_node_color(file="~/Bioinformatics_ExternalData/GeneOntology/go_colors_biological.csv", ):
         go_colors = pd.read_csv(file)
 
         def selectgo(x):
@@ -159,60 +159,12 @@ class Ontology(Database):
         go_colors["node"] = go_colors[[
             col for col in go_colors.columns if col.isdigit()
         ]].apply(selectgo, axis=1)
-        go_id_colors = go_colors[go_colors["node"].notnull()].set_index(
-            "node")["HCL.color"]
-        go_id_colors = go_id_colors[~go_id_colors.index.duplicated(
-            keep="first")]
-        print(go_id_colors.unique().shape,
-              go_colors["HCL.color"].unique().shape)
+        go_id_colors = go_colors[go_colors["node"].notnull()].set_index("node")["HCL.color"]
+        go_id_colors = go_id_colors[~go_id_colors.index.duplicated(keep="first")]
+
+        print(go_id_colors.unique().shape, go_colors["HCL.color"].unique().shape)
         return go_id_colors
 
-
-
-class HumanPhenotypeOntology(Ontology):
-    """Loads the Human Phenotype Ontology database from https://hpo.jax.org/app/ .
-
-        Default path: "http://geneontology.org/gene-associations/" .
-        Default file_resources: {
-            "hp.obo": "http://purl.obolibrary.org/obo/hp.obo",
-        }
-        """
-    COLUMNS_RENAME_DICT = {}
-
-    def __init__(
-        self,
-        path="https://hpo.jax.org/",
-        file_resources=None,
-        col_rename=COLUMNS_RENAME_DICT,
-        blocksize=0,
-        verbose=False,
-    ):
-        """
-        Handles downloading the latest Human Phenotype Ontology obo and annotation data, preprocesses them. It provide
-        functionalities to create a directed acyclic graph of Ontology terms, filter terms, and filter annotations.
-        """
-        if file_resources is None:
-            file_resources = {
-                "hp.obo": "http://purl.obolibrary.org/obo/hp.obo",
-            }
-        super().__init__(
-            path,
-            file_resources,
-            col_rename=col_rename,
-            blocksize=blocksize,
-            verbose=verbose,
-        )
-
-    def info(self):
-        print("network {}".format(nx.info(self.network)))
-
-    def load_network(self, file_resources):
-        for file in file_resources:
-            if ".obo" in file:
-                network = obonet.read_obo(file_resources[file])
-                network = network.reverse(copy=True)
-                node_list = np.array(network.nodes)
-        return network, node_list
 
 
 class GeneOntology(Ontology):
@@ -317,39 +269,33 @@ class GeneOntology(Ontology):
 
         return network, node_list
 
-    def split_annotations(self, src_node_col="gene_name", dst_node_col="go_id", train_date: str = "2017-06-15",
-                          valid_date: str = "2017-11-15", test_date: str = "2021-12-31",
-                          filter_evidence: List = ['EXP', 'IDA', 'IPI', 'IMP', 'IGI', 'IEP', 'TAS', 'IC'],
+    def split_annotations(self, src_node_col="gene_name", dst_node_col="go_id",
+                          train_date="2017-06-15", valid_date="2017-11-15", test_date="2021-12-31",
+                          filter_evidence: List[str] = ['EXP', 'IDA', 'IPI', 'IMP', 'IGI', 'IEP', 'TAS', 'IC'],
                           groupby: List[str] = ["Qualifier"],
-                          filter_dst_nodes: Union[List, pd.Index] = None,
+                          filter_dst_nodes: pd.Index = None,
                           agg: Union[Callable, str] = "unique") -> Tuple[DataFrame, DataFrame, DataFrame]:
         assert isinstance(groupby, list)
         if src_node_col not in groupby:
+            # Set the source column (i.e. protein_id or gene_name), to be the first in groupby
             groupby = [src_node_col] + groupby
         if "Qualifier" not in groupby:
             groupby.append("Qualifier")
 
         if agg == "add_parent":
+            subgraph = self.get_subgraph(edge_types="is_a")
+
             if isinstance(self.annotations, dd.DataFrame):
                 agg = dd.Aggregation(name='_unique_add_parent',
-                                     chunk=lambda s: s.apply(lambda x: list(set(x))),
-                                     agg=lambda s0: s0.obj + get_predecessor_terms(self.get_subgraph(edge_types="is_a"),
-                                                                                   s0.obj))
+                                     chunk=lambda s: s.unique(),
+                                     agg=lambda s0: s0.apply(get_predecessor_terms, subgraph, keep_terms=True))
             else:
-                def _unique_add_parent(s: pd.Series) -> List:
-                    if s.empty: return
-                    s = s.unique()
-                    return s.tolist() + get_predecessor_terms(self.get_subgraph(edge_types="is_a"), s).iloc[0]
+                agg = lambda s: get_predecessor_terms(s, subgraph, keep_terms=True)
 
-                agg = _unique_add_parent
-        else:
-            assert agg in ['unique', 'add_parent']
-
-        # Set the source column (i.e. protein_id or gene_name), to be the first in groupby
         def _remove_dup_neg_go_id(s: pd.Series) -> pd.Series:
             if s.isna().any():
                 pass
-            elif isinstance(s[neg_dst_col], (list, np.ndarray)) and isinstance(s[dst_node_col], (list, np.ndarray)):
+            elif isinstance(s[neg_dst_col], Iterable) and isinstance(s[dst_node_col], Iterable):
                 rm_dups_go_id = [go_id for go_id in s[neg_dst_col] if go_id not in s[dst_node_col]]
                 if len(rm_dups_go_id) == 0:
                     rm_dups_go_id = None
@@ -359,7 +305,9 @@ class GeneOntology(Ontology):
         neg_dst_col = f"neg_{dst_node_col}"
 
         # Filter annotations
-        annotations = self.annotations[self.annotations["Evidence"].isin(filter_evidence)]
+        annotations = self.annotations
+        if filter_evidence is not None:
+            annotations = annotations[annotations["Evidence"].isin(filter_evidence)]
         if filter_dst_nodes is not None:
             annotations = annotations[annotations[dst_node_col].isin(filter_dst_nodes)]
 
@@ -375,32 +323,46 @@ class GeneOntology(Ontology):
 
         outputs = []
         for anns in [train_anns, valid_anns, test_anns]:
+            # Keep track of which annotation has a "NOT" Qualifier
             is_neg_ann = anns["Qualifier"].map(lambda li: "NOT" in li)
 
             # Convert `Qualifiers` entries of list of strings to string
-            anns['Qualifier'] = anns['Qualifier'].apply(lambda li: "".join([i for i in li if i != "NOT"]))
+            args = dict(meta=pd.Series([""])) if isinstance(anns, dd.DataFrame) else {}
+            anns['Qualifier'] = anns['Qualifier'].apply(lambda li: "".join([i for i in li if i != "NOT"]), **args)
 
-            # Positive gene-GO annotations
-            if isinstance(anns, pd.DataFrame):
+            # Aggregate gene-GO annotations
+            if isinstance(anns, pd.DataFrame) and len(anns.index):
                 pos_anns = anns[~is_neg_ann].groupby(groupby).agg(**{dst_node_col: (dst_node_col, agg)})
                 neg_anns = anns[is_neg_ann].groupby(groupby).agg(**{neg_dst_col: (dst_node_col, agg)})
+                pos_neg_anns = pd.concat([pos_anns, neg_anns], axis=1)
 
-            elif isinstance(anns, dd.DataFrame):
+            elif isinstance(anns, dd.DataFrame) and len(anns.index):
                 pos_anns = anns[~is_neg_ann].groupby(groupby).agg({dst_node_col: agg})
-                neg_anns = anns[is_neg_ann].groupby(groupby).agg({neg_dst_col: agg})
+                if is_neg_ann.any().compute():
+                    neg_anns = anns[is_neg_ann].groupby(groupby).agg({dst_node_col: agg})
+                    neg_anns.columns = [neg_dst_col]
+                    pos_neg_anns = dd.concat([pos_anns, neg_anns], axis=1)
+                else:
+                    pos_neg_anns = pos_anns
+                    pos_neg_anns[neg_dst_col] = None
 
-            # Negative gene-GO annotations
-            pos_neg_anns = pd.concat([pos_anns, neg_anns], axis=1)
-            pos_neg_anns.drop(index=[""], inplace=True, errors="ignore")
+            if isinstance(pos_neg_anns, pd.DataFrame):
+                pos_neg_anns = pos_neg_anns.drop([""], axis='index', errors="ignore")
 
             # Remove "GO:0005515" (protein binding) annotations for a gene if it's the gene's only annotation
-            _exclude_single_fn = lambda li: \
-                (len(li) == 1 and "GO:0005515" in li) if isinstance(li, (list, np.ndarray)) else False
-            pos_neg_anns.loc[pos_neg_anns[dst_node_col].map(_exclude_single_fn), dst_node_col] = None
+            _exclude_single_fn = lambda li: None \
+                if isinstance(li, Iterable) and len(li) == 1 and "GO:0005515" in li else li
+            args = dict(meta=pd.Series([list()])) if isinstance(anns, dd.DataFrame) else {}
+            pos_neg_anns[dst_node_col] = pos_neg_anns[dst_node_col].apply(_exclude_single_fn, **args)
+
             # Drop rows with allna values
-            pos_neg_anns.drop(index=pos_neg_anns.index[pos_neg_anns.isna().all(1)], inplace=True)
+            if isinstance(pos_neg_anns, pd.DataFrame):
+                pos_neg_anns = pos_neg_anns.drop(pos_neg_anns.index[pos_neg_anns.isna().all(1)], axis='index')
+
             # Ensure no negative terms duplicates positive annotations
-            pos_neg_anns = pos_neg_anns.apply(_remove_dup_neg_go_id, axis=1)
+            args = dict(meta=pd.DataFrame({dst_node_col: [], neg_dst_col: []})) if isinstance(anns,
+                                                                                              dd.DataFrame) else {}
+            pos_neg_anns = pos_neg_anns.apply(_remove_dup_neg_go_id, axis=1, **args)
 
             outputs.append(pos_neg_anns)
 
@@ -433,13 +395,12 @@ class UniProtGOA(GeneOntology):
         verbose=False,
     ):
         """
-        Loads the GeneOntology database from http://geneontology.org .
+        Loads the UniProtGOA database from https://www.ebi.ac.uk/GOA/ .
 
             Default path: "ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/UNIPROT/" .
             Default file_resources: {
+                "goa_uniprot.gaf.gz": "goa_uniprot.gaf.gz",
                 "go.obo": "http://current.geneontology.org/ontology/go.obo",
-                "goa_uniprot.gpi": "goa_uniprot.gpi.gz",
-                "goa_uniprot.gaf": "goa_uniprot.gaf.gz",
             }
 
         Handles downloading the latest Gene Ontology obo and annotation data, preprocesses them. It provide
@@ -456,12 +417,17 @@ class UniProtGOA(GeneOntology):
             file_resources = {
                 "go.obo": "http://current.geneontology.org/ontology/go.obo",
                 f"goa_{self.species.lower()}.gaf.gz": os.path.join(species, f"goa_{substr}.gaf.gz"),
-                # f"goa_{self.species.lower()}_isoform.gaf": os.path.join(species, f"goa_{substr}_isoform.gaf.gz"),
-                # f"goa_{self.species.lower()}_complex.gaf": os.path.join(species, f"goa_{substr}_complex.gaf.gz"),
-                # f"goa_{self.species.lower()}.gpi": os.path.join(species, f"goa_{substr}.gpi.gz"),
+                # f"goa_{self.species.lower()}_isoform.gaf.gz": os.path.join(species, f"goa_{substr}_isoform.gaf.gz"),
+                # f"goa_{self.species.lower()}_complex.gaf.gz": os.path.join(species, f"goa_{substr}_complex.gaf.gz"),
             }
-        super().__init__(path=path, species=species, file_resources=file_resources, col_rename=col_rename,
-                         blocksize=blocksize, verbose=verbose)
+
+        if not any('.obo' in file for file in file_resources):
+            warnings.warn(
+                f'No .obo file provided in `file_resources`, so automatically adding "http://purl.obolibrary.org/obo/go/go-basic.obo"')
+            file_resources["go-basic.obo"] = "http://purl.obolibrary.org/obo/go/go-basic.obo"
+
+        super(GeneOntology, self).__init__(path=path, file_resources=file_resources, col_rename=col_rename,
+                                           blocksize=blocksize, verbose=verbose)
 
 
 class InterPro(Ontology):
@@ -589,22 +555,79 @@ class InterPro(Ontology):
         return pd.DataFrame(tuples, columns=['ENTRY_AC', "go_id", "go_desc"])
 
 
-def get_predecessor_terms(g: nx.MultiDiGraph, anns: pd.Series) -> pd.Series:
-    if isinstance(anns, np.ndarray):
-        anns = pd.Series([anns.tolist()])
-    elif isinstance(anns, list):
-        anns = pd.Series([anns])
-    elif isinstance(anns, pd.Series) and (~anns.map(type).isin({list, np.ndarray})).any():
-        anns = anns.map(list)
+class HumanPhenotypeOntology(Ontology):
+    """Loads the Human Phenotype Ontology database from https://hpo.jax.org/app/ .
 
-    try:
-        parent_terms = anns.map(
-            lambda li: list({parent for term in li for parent in nx.ancestors(g, term)}) \
-                if isinstance(li, (list, np.ndarray)) else [])
+        Default path: "http://geneontology.org/gene-associations/" .
+        Default file_resources: {
+            "hp.obo": "http://purl.obolibrary.org/obo/hp.obo",
+        }
+        """
+    COLUMNS_RENAME_DICT = {}
 
-    except Exception as e:
-        print(e)
-        return anns
+    def __init__(
+        self,
+        path="https://hpo.jax.org/",
+        file_resources=None,
+        col_rename=COLUMNS_RENAME_DICT,
+        blocksize=0,
+        verbose=False,
+    ):
+        """
+        Handles downloading the latest Human Phenotype Ontology obo and annotation data, preprocesses them. It provide
+        functionalities to create a directed acyclic graph of Ontology terms, filter terms, and filter annotations.
+        """
+        if file_resources is None:
+            file_resources = {
+                "hp.obo": "http://purl.obolibrary.org/obo/hp.obo",
+            }
+        super().__init__(
+            path,
+            file_resources,
+            col_rename=col_rename,
+            blocksize=blocksize,
+            verbose=verbose,
+        )
+
+    def info(self):
+        print("network {}".format(nx.info(self.network)))
+
+    def load_network(self, file_resources):
+        for file in file_resources:
+            if ".obo" in file:
+                network = obonet.read_obo(file_resources[file])
+                network = network.reverse(copy=True)
+                node_list = np.array(network.nodes)
+        return network, node_list
+
+
+def get_predecessor_terms(anns: Union[pd.Series, Iterable], g: nx.MultiDiGraph, keep_terms=True) \
+    -> Union[pd.Series, List[str]]:
+    def _get_ancestors(terms: Iterable):
+        try:
+            parents = list({parent for term in terms \
+                            for parent in nx.ancestors(g, term)} if isinstance(terms, Iterable) else [])
+            if keep_terms and isinstance(terms, Iterable):
+                parents.extend(terms)
+
+        except NetworkXError as nxe:
+            print("get_predecessor_terms._get_ancestors:", nxe)
+            parents = terms if keep_terms else []
+
+        return parents
+
+    if isinstance(anns, pd.Series):
+        if (anns.map(type) == str).any():
+            anns = anns.map(lambda s: [s])
+        # if (~anns.map(type).isin({list, np.ndarray})).any():
+        #     anns = anns.map(list)
+
+        parent_terms = anns.map(_get_ancestors)
+
+    elif isinstance(anns, Iterable):
+        parent_terms = _get_ancestors(anns)
+    else:
+        parent_terms = []
 
     return parent_terms
 
